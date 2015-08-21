3d9819cef14295a22a3166588a40b4b4770b6ec7
Alex Birch <birch@Alexs-MacBook-Pro.local>
2015-08-22 00:24:15 +0100

    working nicely

--- src/main/clojure/project.clj
+++ src/main/clojure/project.clj
@@ -1,5 +1,5 @@
 (defproject PleaseWork "1.0.0-SNAPSHOT"
 						:description "Hello, Clojure"
-						:dependencies [[org.clojure/clojure "1.6.0"]]
+						:dependencies [[org.clojure/clojure "1.7.0"]]
 						:aot [PleaseWork.net.billforward]
 						:main PleaseWork.net.billforward)
