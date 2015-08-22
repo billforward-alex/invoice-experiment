0dbcbd0673915ba090b3261760553d290de34aad
Alex Birch <birch@Alexs-MacBook-Pro.local>
2015-08-22 13:54:24 +0100

    invoice class

---
+++ src/main/java/net/billforward/Invoicey.java
@@ -0,0 +1,16 @@
+package net.billforward;
+
+/**
+ * Created by birch on 22/08/2015.
+ */
+public class Invoicey {
+    private String id;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
