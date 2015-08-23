f9de749d7f723f06485c97ca32fed91442a1fd2f
Alex Birch <birch@Alexs-MacBook-Pro.local>
2015-08-23 13:26:04 +0100

    delete

--- grunter/src/main/java/net/billforward/App.java
+++ grunter/src/main/java/net/billforward/App.java
@@ -1,13 +1,5 @@
 package net.billforward;
 
-import io.apigee.trireme.core.NodeEnvironment;
-import io.apigee.trireme.core.NodeException;
-import io.apigee.trireme.core.NodeScript;
-import io.apigee.trireme.core.ScriptStatus;
-
-import java.io.File;
-import java.util.concurrent.ExecutionException;
-
 /**
  * Hello world!
  *
@@ -22,36 +14,5 @@ public class App
 
     public void main() {
         System.out.println( "Hello World!" );
-
-//        // The NodeEnvironment controls the environment for many scripts
-//        NodeEnvironment env = new NodeEnvironment();
-//
-//        // Get current classloader
-//        ClassLoader classLoader = this.getClass().getClassLoader();
-//        String path = classLoader.getResource("js/index.js").getFile();
-//
-//        // Pass in the script file name, a File pointing to the actual script, and an Object[] containg "argv"
-//        NodeScript script = null;
-//        try {
-//            script = env.createScript("index.js",
-//                    new File(path), null);
-//        } catch (NodeException e) {
-//            e.printStackTrace();
-//        }
-//
-//        // Wait for the script to complete
-//        ScriptStatus status = null;
-//        try {
-//            status = script.execute().get();
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        } catch (ExecutionException e) {
-//            e.printStackTrace();
-//        } catch (NodeException e) {
-//            e.printStackTrace();
-//        }
-//
-//        // Check the exit code
-//        System.exit(status.getExitCode());
     }
 }
